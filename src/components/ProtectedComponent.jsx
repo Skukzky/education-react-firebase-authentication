b65f8c87992a1d727e029faac374cc7bb5cb4b59
Skukzky <rifkioskar1@gmail.com>
2022-07-13 15:01:48 +0700

    update

--- src/components/ProtectedComponent.jsx
+++ src/components/ProtectedComponent.jsx
@@ -1,16 +1,16 @@
 // Sekarang pada ProtectedComponent, kita tetap harus menggunakan useEffect
 // Agar terlepas dari Warning yang diberikan
 // (useNavigate harus di dalam useEffect)
-import React, { useEffect } from "react";
+import { useEffect } from 'react';
 
 // Di sini kita akan akan menggunakan hooks:
 // - untuk mendeteksi user sudah login (useAuthState)
 // - untuk memaksa navigasi bila user belum login dengan (useNavigate)
-import { useNavigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 // Untuk bisa menggunakan useAuthState, kita membutuhkan auth dari authentication/firebase
-import { auth } from "../authentication/firebase";
+import { auth } from '../authentication/firebase';
 
 // Karena di sini akan nge-slot, maka harus menerima props children
 const ProtectedComponent = ({ children }) => {
@@ -24,7 +24,7 @@ const ProtectedComponent = ({ children }) => {
     // Di sini kita akan membuat logic, apabila user tidak ada (null), maka akan kita
     // "paksa" ke halaman login
     if (!user) {
-      navigate("/login");
+      navigate('/login');
       return;
     }
   }, [user, navigate]);
